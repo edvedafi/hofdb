96e66eed147e39b373b38fa2bc8baf595ebf3918
edvedafi <jburich@gmail.com>
2023-06-06 08:46:03 -0500

    firebase deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "sveltekit-adapter-firebase";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+export default {
+  kit: {
+   adapter: adapter(),
+  },
 };
-
-export default config;
